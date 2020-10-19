2986dcaf42eed5f40cbe5ac23b70d4b0bf7f3e09
mralimov <mralimov89@gmail.com>
2020-10-18 22:44:01 -0400

    IMage updates

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,5 +1,5 @@
 let resumeData = {
-  imagebaseurl: 'https://joshkuruvilla512.github.io/',
+  imagebaseurl: './images/Portfolio.png',
   name: 'Donnie Alimov',
   role: 'Full Stack Developer',
   linkedinId: 'Your LinkedIn Id',
@@ -25,7 +25,7 @@ let resumeData = {
   aboutme:
     'I am a Full Stack Developer with background experience in the Automotive Industry',
   address: 'Ottawa, ON',
-  website: 'https://react-portfolio-ut.herokuapp.com/',
+  website: 'https://serene-shelf-43521.herokuapp.com/',
   education: [
     {
       UniversityName: 'Carleton University',
@@ -34,37 +34,15 @@ let resumeData = {
       YearOfPassing: '2020',
       Achievements: 'Certificate',
     },
-    {
-      UniversityName: 'Carleton University',
-      specialization: 'Marketing',
-      MonthOfPassing: 'April',
-      YearOfPassing: '2017',
-      Achievements: 'Bachelors',
-    },
   ],
   work: [
     {
-      CompanyName: 'Mercedes',
+      CompanyName: 'Mercedes-Benz',
       specialization: 'Product Specialist',
       MonthOfLeaving: 'December',
       YearOfLeaving: '2011-November 2019',
       Achievements:
-        'Worked to make sure that the parts and equipment are delivered to the installation team on time and in the condition required for proper installation',
-      /* <li>
-          <p>Works to make sure that the parts and equipment are delivered to
-        the installation team on time and in the condition required for proper installation</p>
-        </li>
-        <li>
-          <p>Participated in Pre Installation Planning/Scheduling meetings to develop the logistics plan</p>
-        </li>
-        <li>
-          <p>Used SAP for tracking/identifying parts on arrival at site and properly label parts so they can be located in the installation plan
-            </p>
-        </li>
-        <li>
-      <p>Trained People in Durr’s Logistics Management procedures</p>
-        </li>
-      </ul> */
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
   ],
   skillsDescription: '',
@@ -81,39 +59,27 @@ let resumeData = {
   ],
   portfolio: [
     {
-      name: 'Colossal Hogs',
-      description: 'The PACK IS BACK',
-      imgurl: './images/ColossalHogs.png',
-      imglink: 'https://the-pack-is-back.herokuapp.com/',
+      name: 'My Portfolio',
+      description: 'Main portfolio',
+      imgurl: './images/Portfolio.png',
+      imglink: 'https://mralimov.github.io/My-Portfolio/',
     },
     {
-      name: 'Team Generator',
-      description: 'Generates a team',
-      imgurl: './images/TeamGeneratorProfile.png',
-      imglink: 'https://github.com/mralimov/React-Employee-Directory',
-    },
-    {
-      name: 'ATX Now!',
-      description: 'Finding concerts in Austin',
-      imgurl: './images/ATXNow.png',
+      name: 'Groceteria',
+      description: 'Grocery Store',
+      imgurl: './images/Groceteria.PNG',
       imglink: 'https://kgeary.github.io/atxnow/',
     },
     {
-      name: 'Trivia Quiz',
-      description: 'An Office themed Trivia Quiz',
-      imgurl: 'images/TrivaQuiz.png',
-      imglink: 'https://github.com/joshkuruvilla512/Trivia-Quiz',
-    },
-    {
-      name: 'Second Portfolio Page with Bootstrap',
-      description: 'Introduced to Bootstrap',
-      imgurl: './images/PortfolioBootstrap.png',
+      name: 'Password Generator',
+      description: 'Random password generator',
+      imgurl: './images/Password Generator JS.PNG',
       imglink: 'https://github.com/joshkuruvilla512/Homework2',
     },
     {
-      name: 'First Portfolio Page',
-      description: 'Building blocks to coding',
-      imgurl: './images/Portfolio.png',
+      name: 'Productivity Booster',
+      description: 'Office productivity app',
+      imgurl: './images/Productivity booster.PNG',
       imglink: 'https://github.com/joshkuruvilla512/JoshKuruvilla.github.io',
     },
   ],
